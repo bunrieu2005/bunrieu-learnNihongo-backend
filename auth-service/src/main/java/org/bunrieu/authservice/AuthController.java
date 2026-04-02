55a270199f4ca1e1c1479914d1a97eff3f781e23
bunrieu2005 <trvtan503@gmail.com>
2026-04-02 22:14:59 +0700

    feat: add api-gateway and auth-service with eureka registration

---
+++ auth-service/src/main/java/org/bunrieu/authservice/AuthController.java
@@ -0,0 +1,15 @@
+package org.bunrieu.authservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello , port 8081 from auth service";
+    }
+}
+
